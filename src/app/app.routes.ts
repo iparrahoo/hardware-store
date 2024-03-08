98fe6822160cdb33b24ddb274438809271c1978c
iparrahoo <iparrahoo@object-ive.com>
2024-03-09 01:32:43 +0400

    Init firebase

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { SignInComponent } from './pages/sign-in/sign-in.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'sign-in', component: SignInComponent },
+    { path: '',   redirectTo: '/sign-in', pathMatch: 'full' }
+];
